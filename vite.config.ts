c24ad890e1b7abe347adb0de43c074992fe71521
jrs <nil>
2025-10-14 14:54:10 -0400

    hosting works

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,18 @@
 import { defineConfig } from "vite"
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: (id) => {
+                    console.log(id)
+                    if (id.includes('node_modules')) {
+                        return id.split("node_modules")[1].split("/")[0];
+                    }
+
+                    return null;
+                }
+            }
+        }
+    }
 })
